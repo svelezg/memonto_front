fb53f4f088867d0814dcea201d2b119163153527
Cesar Velez <cavb.28@gmail.com>
2020-03-12 13:38:38 -0500

    Added: Components: averages, getplates, getVehicleInfo, plate, plates, platesAll, reviews, userSigin, vehicles Views: Home, vehicles, vehiclesAll

--- src/components/averages.js
+++ src/components/averages.js
@@ -6,7 +6,7 @@ const Averages = ({ average }) => {
   }
   return (
     <div>
-      <center><h1>Rating</h1></center>
+      <center><h1>Rating </h1></center>
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">{average.Average}</h5>
